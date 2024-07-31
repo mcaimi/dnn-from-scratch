5c434f983a451f30c16ac3ec2d0bc3e407c96177
Marco Caimi <mcaimi@redhat.com>
2024-07-31 16:13:56 +0200

    Renamed test files

---
+++ C/tests/allocate-deallocate.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "linear_layer.h"
+#include "sigmoid_layer.h"
+
+#define CASES 6
+
+struct {
+  unsigned int input;
+  unsigned int output;
+  double lr;
+} test_cases[] = { { .input = 16, .output = 32, .lr = 0.00004 },
+                   { .input = 32, .output = 32, .lr = 0.00004 },
+                   { .input = 32, .output = 64, .lr = 0.00004 },
+                   { .input = 64, .output = 256, .lr = 0.00004 },
+                   { .input = 256, .output = 128, .lr = 0.00004 },
+                   { .input = 256, .output = 10, .lr = 0.00004 } };
+
+int main(int argc, char **argv) {
+  linear *l_test;
+  sigmoid *s_test;
+
+ for (unsigned int i=0; i<CASES; i++) {
+   printf("-= [Linear Layer] %s [%d] =-\n", "Running case ", i);
+   l_test = (linear *)linearCreate(test_cases[i].input, test_cases[i].output, test_cases[i].lr);
+   linearInfo(l_test);
+   linearFree(l_test);
+ }
+
+ for (unsigned int i=0; i<CASES; i++) {
+   printf("-= [Sigmoid Layer] %s [%d] =-\n", "Running case ", i);
+   s_test = (sigmoid *)sigmoidCreate(test_cases[i].output);
+   sigmoidInfo(s_test);
+   sigmoidFree(s_test);
+ }
+}
