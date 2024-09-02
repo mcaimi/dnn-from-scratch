637a8579320496a7cca440724a7ecf6395d5b9d2
Marco Caimi <mcaimi@redhat.com>
2024-09-02 15:09:21 +0200

    Update tests

--- C/tests/forward-backward.c
+++ C/tests/forward-backward.c
@@ -3,6 +3,7 @@
 #include "matrix.h"
 #include "linear_layer.h"
 #include "relu_layer.h"
+#include "sigmoid_layer.h"
 #include "mnist.h"
 
 #define INPUTS 768
@@ -19,7 +20,9 @@ int main(int argc, char **argv) {
   }
 
   linear *l_test_input;
+  relu *r_input;
   linear *l_test_hidden;
+  relu *r_hidden;
   linear *l_test_output;
   relu *s_output;
 
@@ -32,11 +35,16 @@ int main(int argc, char **argv) {
   l_test_input = (linear *)linearCreate(INPUTS, HIDDEN1, LR);
   l_test_hidden = (linear *)linearCreate(HIDDEN1, HIDDEN2, LR);
   l_test_output = (linear *)linearCreate(HIDDEN2, OUTPUTS, LR);
-  printf("-= [relu Layer] [%d] =-\n", OUTPUTS);
-  s_output = (relu *)reluCreate(OUTPUTS);
+  printf("-= [relu Layer] [%d] =-\n", HIDDEN1);
+  r_input = (relu *)reluCreate(HIDDEN1);
+  r_hidden = (relu *)reluCreate(HIDDEN2);
+  printf("-= [sigmoid Layer] [%d] =-\n", OUTPUTS);
+  s_output = (relu *)sigmoidCreate(OUTPUTS);
   linearInfo(l_test_input);
   linearInfo(l_test_hidden);
   linearInfo(l_test_output);
+  reluInfo(r_input);
+  reluInfo(r_hidden);
   reluInfo(s_output);
 
   // make a prediction
@@ -48,26 +56,26 @@ int main(int argc, char **argv) {
 
   // input
   linearFeedIn(l_test_input, input_vector);
-  double *input_forward_pass = linearFeedForward(l_test_input);
+  reluFeedIn(r_input, linearFeedForward(l_test_input));
+  double *input_forward_pass = reluFeedForward(r_input);
 
   // hidden
   linearFeedIn(l_test_hidden, input_forward_pass);
-  double *hidden_forward_pass = linearFeedForward(l_test_hidden);
+  reluFeedIn(r_hidden, linearFeedForward(l_test_hidden));
+  double *hidden_forward_pass = reluFeedForward(r_hidden);
 
   // output
   linearFeedIn(l_test_output, hidden_forward_pass);
-  double *output_forward_pass = linearFeedForward(l_test_output);
+  reluFeedIn(s_output, linearFeedForward(l_test_output));
+  double *output_forward_pass = reluFeedForward(s_output);
 
-  // relu
-  reluFeedIn(s_output, output_forward_pass);
-  double *output_relu = reluFeedForward(s_output);
-  printf("--> [FORWARD PASS] : Out Vector (0x%X):\n", output_relu);
-  displayWeights(&output_relu, 1, OUTPUTS);
+  printf("--> [FORWARD PASS] : Out Vector (0x%X):\n", (unsigned int)output_forward_pass);
+  displayWeights(&output_forward_pass, 1, OUTPUTS);
 
   // simulate gradient computation
   double *prev_grad = constantVector(OUTPUTS, 0.0f);
   for (unsigned int n=0; n<OUTPUTS; n++){
-    double diffval = output_relu[n] - target_vector[n];
+    double diffval = output_forward_pass[n] - target_vector[n];
     prev_grad[n] = diffval;
   }
 
@@ -79,11 +87,13 @@ int main(int argc, char **argv) {
   double *o_grads = linearBackPropagation(l_test_output, so_grads);
 
   // hidden
-  double *h_grads = linearBackPropagation(l_test_hidden, o_grads);
+  double *sh_grads = reluBackPropagation(r_hidden, o_grads);
+  double *h_grads = linearBackPropagation(l_test_hidden, sh_grads);
   free(o_grads);
 
   // input
-  double *i_grads = linearBackPropagation(l_test_input, h_grads);
+  double *si_grads = reluBackPropagation(r_input, h_grads);
+  double *i_grads = linearBackPropagation(l_test_input, si_grads);
   free(h_grads);
 
   printf("--> [BACKPROPAGATION] : Out Vector:\n");
@@ -93,6 +103,8 @@ int main(int argc, char **argv) {
   linearFree(l_test_input);
   linearFree(l_test_hidden);
   linearFree(l_test_output);
+  reluFree(r_input);
+  reluFree(r_hidden);
   reluFree(s_output);
   mnistFreeData(data);
   mnistFreeIndex(labels);
